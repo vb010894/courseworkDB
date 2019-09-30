6b7d42d6557698f7a64be32fc499f7d197e5db63
Vladimir Zinovyev <vb010894@gmail.com>
2019-09-30 22:45:15 +0300

    OrganizationTable RESTfull add

--- src/main/java/root/Organization/OrganizationController.java
+++ src/main/java/root/Organization/OrganizationController.java
@@ -1,24 +1,57 @@
 package root.Organization;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/_apis")
 public class OrganizationController
 {
-    @RequestMapping(value="/org", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
-    public String getOrganization()
+    @RequestMapping(value="/org/{id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public String getOrganization
+            (
+                    @PathVariable String id,
+                    @RequestParam int start,
+                    @RequestParam int page,
+                    @RequestParam int limit
+            )
     {
-        return "{total:1,state:true}";
+        return new OrganizationModel().getOrganization(start, page, limit, id);
+    }
+
+    @RequestMapping(value="/org/{id}", method = RequestMethod.DELETE, produces = "application/json; charset=utf-8")
+    public String deleteOrganization
+            (
+                    @PathVariable String id
+            )
+    {
+        return new OrganizationModel().deleteOrganization(id).toString();
+    }
+
+    @RequestMapping(value="/org/{id}", method = RequestMethod.PUT, produces = "application/json; charset=utf-8")
+    public String updateOrganization
+            (
+                    @RequestBody OrganizationModel model,
+                    @PathVariable String id
+            )
+    {
+        return model.updateOrganization(id).toString();
+    }
+
+    @RequestMapping(value="/org", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    public String getOrganization
+            (
+                    @RequestParam int start,
+                    @RequestParam int page,
+                    @RequestParam int limit
+            )
+    {
+        return new OrganizationModel().getOrganization(start, page, limit, "");
     }
 
     @RequestMapping(value="/org", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
     public String setOrganization(@RequestBody OrganizationModel model)
     {
-        return model.getMess();
+        return model.setOrganization().toString();
     }
 
 }
--- src/main/java/root/Organization/OrganizationModel.java
+++ src/main/java/root/Organization/OrganizationModel.java
@@ -2,6 +2,10 @@ package root.Organization;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import root.core.SqlWork;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class OrganizationModel
 {
@@ -13,13 +17,6 @@ public class OrganizationModel
         this.name = name;
     }
 
-    public String getAdress() {
-        return adress;
-    }
-
-    public void setAdress(String adress) {
-        this.adress = adress;
-    }
 
     public String getInn() {
         return inn;
@@ -37,22 +34,139 @@ public class OrganizationModel
         this.kpp = kpp;
     }
 
+    public String getOgrn() {
+        return ogrn;
+    }
+
+    public void setOgrn(String ogrn) {
+        this.ogrn = ogrn;
+    }
+
+    public String getOktmo() {
+        return oktmo;
+    }
+
+    public void setOktmo(String oktmo) {
+        this.oktmo = oktmo;
+    }
+
     private String name;
-    private String adress;
+    private String ogrn;
     private String inn;
     private String kpp;
+    private String oktmo;
 
-    public String getOrganization(int start, int page, int limit)
+
+    public String getOrganization(int start, int page, int limit, String id)
     {
         JSONObject result = new JSONObject();
-        JSONArray itrms = new JSONArray();
+        JSONArray items = new JSONArray();
+        int count = 0;
 
+        String sqlQuery = (id == "") ? "select * from organization limit " + start +", " + page * limit : "select * from organization where (id = " + id + ") limit " + start +", " + page * limit;
+        System.out.println(sqlQuery);
+        SqlWork sqlWork = new SqlWork();
+        sqlWork.openConnection();
+        ResultSet resultSet = sqlWork.getResult(sqlQuery);
+       try
+       {
+           while (resultSet.next())
+           {
+               count = resultSet.getRow();
+               result.put("idOrganization", resultSet.getString("id"));
+               result.put("name", resultSet.getString("name"));
+               result.put("inn", resultSet.getString("inn"));
+               result.put("kpp", resultSet.getString("kpp"));
+               result.put("oktmo", resultSet.getString("oktmo"));
+               result.put("ogrn", resultSet.getString("ogrn"));
+               items.put(result);
+           }
+       }
+       catch (SQLException sql)
+       {
+           System.out.println();
+           System.out.println("------ОШИБКА------");
+           System.out.println("Сообщение - " + sql);
+           System.out.println("StackTrace - " + sql.getStackTrace());
+       }
+
+       result = new JSONObject();
+       if(count == 0 )
+       {
+           result.put("total", count);
+           result.put("state", false);
+           result.put("items", "[]");
+       }
+       else
+       {
+           result.put("total", count);
+           result.put("state", true);
+           result.put("items", items);
+       }
+       sqlWork.closeConnection();
         return result.toString();
     }
 
-    public void setOrganization()
+    public JSONObject setOrganization()
     {
-
+        JSONObject result = new JSONObject();
+        String sqlQuery = "insert into organization" +
+                "(" +
+                "name," +
+                "inn," +
+                "kpp," +
+                "oktmo," +
+                "ogrn" +
+                ") " +
+                "values" +
+                "('" + this.name + "','" +
+                 this.inn + "','" +
+                 this.kpp + "','" +
+                 this.oktmo + "','" +
+                 this.ogrn + "'" +
+                ")";
+        SqlWork sql = new SqlWork();
+        sql.openConnection();
+        sql.updateQuery(sqlQuery);
+        sql.closeConnection();
+        result.put("State", true);
+        return result;
     }
 
+    public JSONObject deleteOrganization(String id)
+    {
+        JSONObject result = new JSONObject();
+        String sqlQuery = "delete from organization " +
+                "where (" +
+                "id = " + id +
+                ")";
+        System.out.println(sqlQuery);
+        SqlWork sql = new SqlWork();
+        sql.openConnection();
+        sql.updateQuery(sqlQuery);
+        sql.closeConnection();
+        result.put("State", true);
+        return result;
+    }
+
+    public JSONObject updateOrganization(String id)
+    {
+        JSONObject result = new JSONObject();
+        String sqlQuery = "update organization set " +
+                "name = '" + this.name + "'," +
+                "inn = '" + this.inn + "'," +
+                "kpp = '" + this.kpp + "'," +
+                "oktmo = '" + this.oktmo + "'," +
+                "ogrn = '" + this.ogrn + "' " +
+                "where (" +
+                "id = " + id +
+                ")";
+        System.out.println(sqlQuery);
+        SqlWork sql = new SqlWork();
+        sql.openConnection();
+        sql.updateQuery(sqlQuery);
+        sql.closeConnection();
+        result.put("State", true);
+        return result;
+    }
 }
